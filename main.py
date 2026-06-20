43f103a5ae2c7c814b8268d616c7bae9d71a731e
jester-sevetech <jester.sevetech@gmail.com>
2026-06-20 23:03:27 +0800

    Create script for building the app; Create script for automatically setting up backend and frontend app to the client's mini-pc.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import asyncio
 import uvicorn
 
 from app.app_factory import create_app
@@ -5,11 +7,24 @@ from app.core.config import settings
 
 app = create_app()
 
+async def run_migration():
+    import migrate
+    await migrate.main()
+
+async def run_seed():
+    import seed_db
+    await seed_db.main()
+
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host=settings.host,
-        port=settings.port,
-        reload=settings.debug,
-        log_level="debug" if settings.debug else "info",
-    )
+    if len(sys.argv) > 1 and sys.argv[1] == "migrate":
+        asyncio.run(run_migration())
+    elif len(sys.argv) > 1 and sys.argv[1] == "seed":
+        asyncio.run(run_seed())
+    else:
+        uvicorn.run(
+            "main:app",
+            host=settings.host,
+            port=settings.port,
+            reload=settings.debug,
+            log_level="debug" if settings.debug else "info",
+        )
